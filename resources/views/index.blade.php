6fb9c3317ce2121e89367b882f0696f5e4881f27
KuganrajUTM <kuganraj@graduate.utm.my>
2026-07-01 20:32:21 +0800

    changes3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -118,6 +118,17 @@
       background: rgba(0,184,148,0.1);
     }
 
+    .navbar-collapse {
+      display: flex !important;
+      flex-basis: auto !important;
+    }
+    .navbar-nav {
+      flex-direction: row !important;
+    }
+    .navbar-toggler {
+      display: none !important;
+    }
+
     /* ── HERO ────────────────────────────────────── */
     .hero {
       position: relative;
@@ -696,7 +707,7 @@
     <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto align-items-lg-center">
         <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
         <li class="nav-item"><a class="nav-link" href="#features">Features</a></li>
